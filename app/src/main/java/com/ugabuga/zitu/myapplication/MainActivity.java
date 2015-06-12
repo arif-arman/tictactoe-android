7f50972ad5398990d9170f614482bc9d44801963
arif_arman <arif.arman046@gmail.com>
2015-06-12 15:21:49 +0600

    Second commit, mode selection added

--- app/src/main/java/com/ugabuga/zitu/myapplication/MainActivity.java
+++ app/src/main/java/com/ugabuga/zitu/myapplication/MainActivity.java
@@ -96,6 +96,7 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -117,4 +118,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
 }
